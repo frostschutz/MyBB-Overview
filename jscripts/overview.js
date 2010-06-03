e21f7477d73dc4f8ac6374c921118a4691c8fa0b
Andreas Klauer <Andreas.Klauer@metamorpher.de>
2010-06-03 22:42:26 +0200

    cleanup: basic formatting, remove legacy code

--- jscripts/overview.js
+++ jscripts/overview.js
@@ -1,46 +1,32 @@
-// Request erstellen
-function createXMLHttpRequest() {
+/**
+ * This file is part of Overview plugin for MyBB.
+ * Copyright (C) 2005-2009 Michael Schlechtinger <kontakt@mybboard.de>
+ * Copyright (C) 2010 Andreas Klauer <Andreas.Klauer@metamorpher.de>
+ * License: GPLv3
+ */
 
-    var ab = false;
-    // IE 7, Mozilla, Opera, Safari
-    if (typeof(XMLHttpRequest) != 'undefined') {
-        ab = new XMLHttpRequest();
-    }
-    if (!ab) {
-        // IE 6 und älter
-        try {
-            ab  = new ActiveXObject("Msxml2.XMLHTTP");
-        } catch(e) {
-            try {
-                ab  = new ActiveXObject("Microsoft.XMLHTTP");
-            } catch(e) {
-                ab  = false;
-            }
-        }
-    }
-
-    return ab;
-}
-
-
-// Antwort verarbeiten
-function handleResponse() {
-
-    if(req.readyState == 4){
-        var response = req.responseText;
-        var update = new Array();
-        document.getElementById('overview').innerHTML = response;
+// Handle AJAX response
+function overview_response()
+{
+    if(overview.readyState == 4)
+    {
+        document.getElementById('overview').innerHTML = overview.responseText;
         document.getElementById('overview_load').innerHTML = "";
     }
 }
 
-// Daten laden und ausgeben
-function dooverview(wait){
-
-    if (wait == "1"){
+// Load and print data
+function overview_request(wait)
+{
+    if(wait == "1")
+    {
         document.getElementById('overview_load').innerHTML = "<div style=\"text-align: center; margin: 5px auto auto 5px; width: 200px; position: absolute;\"><table style=\"margin: auto auto;\" border=\"0\" cellpadding=\"3\" cellspacing=\"0\" class=\"tborder\"><tr class=\"trow1\"><td><span class=\"smalltext\"><img src=\"images/spinner.gif\" alt=\"Loading\" width=\"12\" height=\"12\" /> Loading...</span></td></tr><table></div>";
     }
-    req.open('GET', 'xmlhttp.php?action=overview');
-    req.onreadystatechange = handleResponse;
-    req.send(null);
+
+    overview = new XMLHttpRequest();
+    overview.open('GET', 'xmlhttp.php?action=overview');
+    overview.onreadystatechange = overview_response;
+    overview.send(null);
 }
+
+/* --- End of file. --- */
