23750b66a14c4f4a27505edea99032f3448611fc
Andreas Klauer <Andreas.Klauer@metamorpher.de>
2010-06-03 22:26:34 +0200

    cleanup: basic formatting

--- inc/languages/english/overview.lang.php
+++ inc/languages/english/overview.lang.php
@@ -1,29 +1,43 @@
 <?php
-###################################
-# Plugin Overview 3.2             #
-# (c) 2005-2009 by MyBBoard.de    #
-# Website: http://www.mybboard.de #
-###################################
+/**
+ * This file is part of Overview plugin for MyBB.
+ * Copyright (C) 2005-2009 Michael Schlechtinger <kontakt@mybboard.de>
+ * Copyright (C) 2010 Andreas Klauer <Andreas.Klauer@metamorpher.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-$l['overview_overview'] = "Overview";
-
-$l['overview_newest_members'] = "Newest members";
-$l['overview_username'] = "Username";
-$l['overview_posts'] = "Posts";
-$l['overview_top_posters'] = "Top poster";
-$l['overview_newest_threads'] = "Newest threads";
-$l['overview_topic'] = "Thread";
 $l['overview_author'] = "Author";
-$l['overview_replies'] = "Replies";
-$l['overview_most_replies'] = "Threads with most replies";
-$l['overview_favourite_threads'] = "Favourite threads";
-$l['overview_views'] = "Views";
-$l['overview_newest_posts'] = "Newest posts";
-$l['overview_subject'] = "Subject";
-$l['overview_newest_polls'] = "Newest polls";
-$l['overview_question'] = "Question";
 $l['overview_bestrep_members'] = "Best reputated mebers";
-$l['overview_reputation'] = "Reputation";
-$l['overview_next_events'] = "Next events";
 $l['overview_event'] = "Event";
+$l['overview_favourite_threads'] = "Favourite threads";
+$l['overview_most_replies'] = "Threads with most replies";
+$l['overview_newest_members'] = "Newest members";
+$l['overview_newest_polls'] = "Newest polls";
+$l['overview_newest_posts'] = "Newest posts";
+$l['overview_newest_threads'] = "Newest threads";
+$l['overview_next_events'] = "Next events";
+$l['overview_overview'] = "Overview";
+$l['overview_posts'] = "Posts";
+$l['overview_question'] = "Question";
+$l['overview_replies'] = "Replies";
+$l['overview_reputation'] = "Reputation";
+$l['overview_subject'] = "Subject";
+$l['overview_top_posters'] = "Top poster";
+$l['overview_topic'] = "Thread";
+$l['overview_username'] = "Username";
+$l['overview_views'] = "Views";
+
 ?>
